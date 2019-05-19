fe9dc9ccaf8466cc2ff0a3f8ae3eb4452b5f3ce5
Michał Huras <michal.huras94@gmail.com>
2019-05-19 20:06:51 +0200

    Poprawa rzutowania venv/visualisation_SPY.py

--- venv/visualisation_SPY.py
+++ venv/visualisation_SPY.py
@@ -44,7 +44,20 @@ if __name__ == '__main__':
 
     print(type(the_image))
 
+    # Rzutowanie wartości na z uint16 na uint8
+    # https://stackoverflow.com/questions/46689428/convert-np-array-of-type-float64-to-type-uint8-scaling-values
+    original_image_type = the_image.dtype
+    original_image_info = np.iinfo(original_image_type)
+    converted_image = the_image.astype(np.float32) / original_image_info.max
+    converted_image_type = np.uint8
+    converted_image_info = np.iinfo(converted_image_type)
+    converted_image = converted_image_info.max * converted_image
+    converted_image = converted_image.astype(np.uint8)
+    print("Original image type: ", original_image_type)
+    print("Converted image type:  ", converted_image_type)
+
     # Wyświetlanie obrazka
+    view = imshow(converted_image, (29, 20, 11))
     view = imshow(the_image, (29, 20, 11))
     print(view)
 
