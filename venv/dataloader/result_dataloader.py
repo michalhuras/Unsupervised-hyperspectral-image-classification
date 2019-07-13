79058fdbb60ea2dcc0850fd2b3fb3bfd35fbafb0
Michał Huras <michal.huras94@gmail.com>
2019-07-13 22:42:56 +0200

    Trsult dataloaer

---
+++ venv/dataloader/result_dataloader.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+'''
+   Result dataloader
+'''
+
+
+class Dataloader():
+    def __init__(self):
+        self.name = 'result_dataloader'
+
+        self.image_shape = ()
+        self.nr_of_clusters = 0
+        self.image = ()
+        self.image_list = ()
+        self.image_labels = ()
+
+    def get_image_labels_from_file(self, input_file_path, verbal=True):
+        if verbal:
+            print()
+            print("***   Get labels from result file   ***")
+            print("---------------------------------")
+
+        image = np.loadtxt(input_file_path, delimiter=" ")
+
+        return image
+
+if __name__ == '__main__':
+    print("RESULT DATALOADER")
