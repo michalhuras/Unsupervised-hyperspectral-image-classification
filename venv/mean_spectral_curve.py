382905550938fb436abbc947488f3bea147b6b6d
Michał Huras <michal.huras94@gmail.com>
2019-07-14 20:28:54 +0200

    Generating spectral curve for all result files

--- venv/mean_spectral_curve.py
+++ venv/mean_spectral_curve.py
@@ -4,23 +4,31 @@ from scipy import io
 import numpy as np
 import time
 
+from drafts.tests.test_dataloader import Dataloader as test_dataloader
+from dataloader.indian_pines_dataloader import Dataloader as indian_pines_dataloader
+from dataloader.samson_dataloader import Dataloader as samson_dataloader
+from dataloader.jasper_ridge_dataloader import Dataloader as jasper_ridge_dataloader
+from dataloader.salinas_dataloader import Dataloader as salinas_dataloader
+from dataloader.salinas_a_dataloader import Dataloader as salinas_a_dataloader
+from dataloader.pavia_dataloader import Dataloader as pavia_dataloader
 
 def create_spectral_curve(
-        dataloader,
+        dataname,
+        image,
+        labels,
+        number_of_labels,
+        shape,
+        results_directory,
         show_img=True,
         save_img=True,
         save_data=True,
         draw_legend=True,
         output_name=""):
 
-    print("\n\n\n\n\n\n")
-    print("----------------")
+    print("\n")
+    print("---------------------------------------------")
     print("START")
-    print("Data name: ", dataloader.get_name())
-    image = dataloader.get_image()
-    labels = dataloader.get_labels()
-    number_of_labels = dataloader.get_number_of_clusters()
-    shape = dataloader.get_image_shape()
+    print("Data name: ", dataname)
 
     elements_in_labels = np.zeros(number_of_labels)
     # zbieram ilość elementów, dla etykiet
@@ -85,7 +93,7 @@ def create_spectral_curve(
             img_name = "IDEAL_spectral_curve.png"
         else:
             img_name = "spectral_curve_" + output_name + ".png"
-        result_img_path = dataloader.get_results_directory() + 'img/' + img_name
+        result_img_path = results_directory + 'img/' + img_name
         print("Path: ", result_img_path)
         plt.savefig(result_img_path, bbox_inches='tight')
         # plt.show()
@@ -97,31 +105,121 @@ def create_spectral_curve(
             data_name = "IDEAL_spectral_curve.txt"
         else:
             data_name = "spectral_curve_" + output_name + ".txt"
-        result_data_path = dataloader.get_results_directory() + 'data/' + data_name
+        result_data_path = results_directory + 'data/' + data_name
         print("Path: ", result_data_path)
         np.savetxt(result_data_path, spectral_curve, delimiter=" ", newline="\n", header=data_name, fmt="%s")
 
     print("END")
+    return spectral_curve
+
+
+def create_spectral_curve_from_dataloader(
+        dataloader,
+        show_img=True,
+        save_img=True,
+        save_data=True,
+        draw_legend=True,
+        output_name=""):
+
+    print("\ncreate_spectral_curve_from_dataloader")
+
+    return create_spectral_curve(
+        dataloader.get_name(verbal=False),
+        dataloader.get_image(verbal=False),
+        dataloader.get_labels(verbal=False),
+        dataloader.get_number_of_clusters(verbal=False),
+        dataloader.get_image_shape(verbal=False),
+        dataloader.get_results_directory(verbal=False),
+        show_img=True,
+        save_img=True,
+        save_data=True,
+        draw_legend=True,
+        output_name="")
+
+def create_spectral_curve_from_dataloader_plus(
+        dataloader,
+        labels,
+        show_img=True,
+        save_img=True,
+        save_data=True,
+        draw_legend=True,
+        output_name=""):
+
+    print("\ncreate_spectral_curve_from_dataloader")
+
+    return create_spectral_curve(
+        dataloader.get_name(verbal=False),
+        dataloader.get_image(verbal=False),
+        labels,
+        dataloader.get_number_of_clusters(verbal=False),
+        dataloader.get_image_shape(verbal=False),
+        dataloader.get_results_directory(verbal=False),
+        show_img=show_img,
+        save_img=save_img,
+        save_data=save_data,
+        draw_legend=draw_legend,
+        output_name=output_name)
+
+def create_spectral_curve_for_ideal_data():
+    create_spectral_curve_from_dataloader(test_dataloader())
+    create_spectral_curve_from_dataloader(indian_pines_dataloader())
+    create_spectral_curve_from_dataloader(samson_dataloader())
+    create_spectral_curve_from_dataloader(jasper_ridge_dataloader())
+    create_spectral_curve_from_dataloader(salinas_dataloader())
+    create_spectral_curve_from_dataloader(salinas_a_dataloader())
+    create_spectral_curve_from_dataloader(pavia_dataloader())
+
+
+def create_spectral_curve_for_existing_data():
+    import os
+
+    print("Searching for result files")
+    result_directories = {
+        "./results/IndianPines/data/": indian_pines_dataloader(),
+        "./results/JasperRidge/data/": jasper_ridge_dataloader(),
+        "./results/Pavia/data/": pavia_dataloader(),
+        "./results/Salinas/data/": salinas_dataloader(),
+        "./results/SalinasA/data/": salinas_a_dataloader(),
+        "./results/Samson/data/": samson_dataloader(),
+        # "./result/tests/data":test_dataloader(),
+    }
+
+    names_and_directories = {}
+    # name: directory
+    for path in result_directories:
+        print("\tPath: ", path)
+        print("\tDataloader name: ", result_directories[path].get_name(False))
+
+        # r=root, d=directories, f = files
+        for r, d, f in os.walk(path):
+            for file in f:
+                if '.txt' in file and "spectral_curve" not in file:
+                    names_and_directories[file] = os.path.join(r, file)
+
+        print(names_and_directories)
+
+        for file_name in names_and_directories:
+            from dataloader.result_dataloader import Dataloader as ResoultDataloader
+            dataloader = ResoultDataloader()
+            image =\
+                dataloader.get_image_labels_from_file(names_and_directories[file_name], verbal=False)
+
+            # import matplotlib.pyplot as plt
+            # plt.imshow(image)
+            # plt.show()
+
+            if file_name.endswith('.txt'):
+                file_name = file_name[:-4]
+            print("\t create_spectral_curve_from_dataloader_plus:  ", file_name)
+            print("\t Dir: ", result_directories[path].get_name(False))
+            create_spectral_curve_from_dataloader_plus(
+                result_directories[path],
+                image,
+                output_name=file_name,
+                show_img=False)
 
 
 if __name__ == '__main__':
-    from drafts.tests.test_dataloader import Dataloader as AAA
-    create_spectral_curve(AAA())
+    # create_spectral_curve_for_ideal_data()
 
-    from dataloader.indian_pines_dataloader import Dataloader as BBB
-    create_spectral_curve(BBB())
-
-    from dataloader.samson_dataloader import Dataloader as CCC
-    create_spectral_curve(CCC())
-
-    from dataloader.jasper_ridge_dataloader import Dataloader as DDD
-    create_spectral_curve(DDD())
-
-    from dataloader.salinas_dataloader import Dataloader as EEE
-    create_spectral_curve(EEE())
-
-    from dataloader.salinas_a_dataloader import Dataloader as FFF
-    create_spectral_curve(FFF())
-
-    from dataloader.pavia_dataloader import Dataloader as GGG
-    create_spectral_curve(GGG())
+    create_spectral_curve_for_existing_data()
