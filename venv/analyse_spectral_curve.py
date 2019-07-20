5d9f60c056129ddcb559022d359fd20076a5b2d3
Michał Huras <michal.huras94@gmail.com>
2019-07-20 18:25:34 +0200

    Analyse spectral curve - creating labels pairs

--- venv/analyse_spectral_curve.py
+++ venv/analyse_spectral_curve.py
@@ -13,6 +13,8 @@ from dataloader.salinas_dataloader import Dataloader as salinas_dataloader
 from dataloader.salinas_a_dataloader import Dataloader as salinas_a_dataloader
 from dataloader.pavia_dataloader import Dataloader as pavia_dataloader
 
+from algorithms.pairing_greedy_algorithm import PairingAlgorithm
+
 
 def count_number_of_labels(ideal_spectral_curve):
     return ideal_spectral_curve.shape[0]
@@ -25,66 +27,7 @@ def plot(img):
     plt.show()
 
 
-def fill_low_matching(matching_clusters, start_row, start_column, labels=[]):
-    # TODO delete
-    # for 3 labels only
-    number_of_labels = 3
-    half_fractional_label = 3
-    for row in range(half_fractional_label):
-        second_row = half_fractional_label + row
-        for i in range(number_of_labels):
-            matching_clusters[start_row + row][start_column + i] = \
-                labels[(row + i) % number_of_labels]
-            matching_clusters[start_row + second_row][start_column + number_of_labels - i - 1] = \
-                labels[(row + i) % number_of_labels]
-
-    return matching_clusters
-
-
-def fill_matching_clusters_initial_values(matching_clusters, number_of_labels, labels=[], start_row=0, start_column=0):
-    # TODO delete
-    if not labels:
-        labels = [label for label in range(number_of_labels)]
-
-    if number_of_labels == 3:
-        fill_low_matching(matching_clusters, 0, 0, labels)
-        return matching_clusters
-
-    row = 0
-    for label in labels:
-        new_labels = labels.copy()
-        new_labels.remove(label)
-        if number_of_labels - 1 == 3:
-            fill_low_matching(matching_clusters, start_row + row, start_column + 1, new_labels)
-        else:
-            fill_matching_clusters_initial_values(
-                matching_clusters,
-                number_of_labels - 1,
-                new_labels,
-                start_row=row,
-                start_column=start_column + 1)
-
-        for i in range(math.factorial(number_of_labels - 1)):
-            matching_clusters[start_row + row][start_column] = label
-            row += 1
-
-    return matching_clusters
-
-if __name__ == '__main__':
-    # Available results files and dataloaders:
-    # "./results/IndianPines/data/"     indian_pines_dataloader()
-    # "./results/JasperRidge/data/"     jasper_ridge_dataloader()
-    # "./results/Pavia/data/"           pavia_dataloader()
-    # "./results/Salinas/data/"         salinas_dataloader()
-    # "./results/SalinasA/data/"        salinas_a_dataloader()
-    # "./results/Samson/data/"          samson_dataloader()
-    # "./result/tests/data"           test_dataloader()
-
-    # Searching result files
-    # Running function for all result files
-
-    print("START")
-
+def pairs_in_spectral_curves(ideal_dataloader, file_name):
     print("* Creating spectral curve loader")
     from dataloader.spectral_curve_dataloader import Dataloader as spectral_curve_dataloader
     dataloader = spectral_curve_dataloader()
@@ -92,7 +35,8 @@ if __name__ == '__main__':
     print()
     print("* Loading ideal spectral curve")
     # ideal_file_name = "./results/Samson/data/IDEAL_spectral_curve.txt"
-    ideal_file_name = "./results/JasperRidge/data/IDEAL_spectral_curve.txt"
+    # ideal_file_name = "./results/JasperRidge/data/IDEAL_spectral_curve.txt"
+    ideal_file_name = ideal_dataloader.get_spectral_curve_directory(verbal=False)
     ideal_spectral_curve = dataloader.get_spectral_curve_from_file(ideal_file_name, verbal=False)
     number_of_labels = count_number_of_labels(ideal_spectral_curve)
     ideal_spectral_curve = ideal_spectral_curve.transpose()
@@ -102,7 +46,7 @@ if __name__ == '__main__':
     print()
     print("* Loading tested spectral curve")
     # file_name = "./results/Samson/data/spectral_curve_clustering_kmeans_linear_autoencoder_1.txt"
-    file_name = "./results/JasperRidge/data/spectral_curve_clustering_kmeans_linear_autoencoder_1.txt"
+    # file_name = "./results/JasperRidge/data/spectral_curve_clustering_kmeans_linear_autoencoder_1.txt"
     spectral_curve = dataloader.get_spectral_curve_from_file(file_name, verbal=False)
     number_of_labels = count_number_of_labels(spectral_curve)
     spectral_curve = spectral_curve.transpose()
@@ -110,21 +54,53 @@ if __name__ == '__main__':
     print("Number of labels: ", number_of_labels)
 
     print()
-    print("* Creating array with matching clusters")
-    print(type(math.factorial(number_of_labels)))
-    print(type(number_of_labels))
+    print("* Pairing labels")
+    pairing_obj = PairingAlgorithm()
+    f_pairs, difference_value =\
+        pairing_obj.math_in_pairs(number_of_labels, ideal_spectral_curve, spectral_curve, prefix="\t", verbal=False)
+    f_pairs = [int(element) for element in f_pairs]
+    print("Pairs: ", f_pairs)
+    print("Difference value: ", difference_value)
 
-    # RODO delete -----
-    number_of_labels = 10
-    matching_clusters = np.zeros((math.factorial(number_of_labels), number_of_labels + 1))
+    print()
+    print("Saving paired labels")
+    results_dir = ideal_dataloader.get_results_directory(verbal=False)
+    result_name = file_name.split("/")[-1]
+    result_name = result_name.split(".")[0]
+    result_name += "_pairs.txt"
+    result_data_path = results_dir + "data/" + result_name
+    print("Result data path:", result_data_path)
+    np.savetxt(result_data_path, f_pairs, delimiter=" ", newline="\n", header=result_name, fmt="%s")
 
-    matching_clusters = fill_matching_clusters_initial_values(matching_clusters, number_of_labels)
-    # matching_clusters = fill_low_matching(matching_clusters, 0, 0)
-    print(matching_clusters)
-    print("Dlugosć: ", len(matching_clusters))
-    # RODO delete ----- end
+    return f_pairs
 
-    # Searching the best result
-    # Saving result
+def compare_with_ground_truth(labeled_image_path, dataloader, pairs):
+    print()
+    print()
+    print("* Compare with ground truth")
+
+    print("* Creating result dataloader")
+    from dataloader.result_dataloader import Dataloader as result_dataloader
+    dataloader = result_dataloader()
+
+
+if __name__ == '__main__':
+    print("START")
+
+    # Available results files and dataloaders:
+    # "./results/IndianPines/data/"     indian_pines_dataloader()
+    # "./results/JasperRidge/data/"     jasper_ridge_dataloader()
+    # "./results/Pavia/data/"           pavia_dataloader()
+    # "./results/Salinas/data/"         salinas_dataloader()
+    # "./results/SalinasA/data/"        salinas_a_dataloader()
+    # "./results/Samson/data/"          samson_dataloader()
+    # "./result/tests/data"           test_dataloader()
+
+    # # Searching result files
+    # # Running function for all result files
+
+    file_name = "./results/JasperRidge/data/spectral_curve_clustering_kmeans_linear_autoencoder_1.txt"
+    pairs = pairs_in_spectral_curves(jasper_ridge_dataloader(), file_name)
+    compare_with_ground_truth(file_name, jasper_ridge_dataloader(), pairs)
 
     print("END")
