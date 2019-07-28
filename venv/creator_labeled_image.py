fe7087b1c18b9a4a4791782c067bdcc09467e1e3
Michał Huras <michal.huras94@gmail.com>
2019-07-28 14:33:33 +0200

    Overwrite function

--- venv/creator_labeled_image.py
+++ venv/creator_labeled_image.py
@@ -20,8 +20,8 @@ import sys
 from models.autoencoder_linear_1 import Autoencoder as Autoencoder1
 from models.autoencoder_linear_2 import Autoencoder as Autoencoder2
 from models.autoencoder_linear_3 import Autoencoder as Autoencoder3
-# from models.autoencoder_none import Autoencoder as Autoencoder4 # TODO !!! puścić
-# from models.autoencoder_convolutional import Autoencoder as Autoencoder5 # TODO !!! puścić
+from models.autoencoder_none import Autoencoder as Autoencoder4 # TODO !!! puścić
+from models.autoencoder_convolutional import Autoencoder as Autoencoder5 # TODO !!! puścić
 
 from dataloader.indian_pines_dataloader import Dataloader as Dataloader1
 from dataloader.indian_pines_cut_out_dataloader import Dataloader as Dataloader11
@@ -86,8 +86,16 @@ def run_machine(
         show_img=True, save_img=True, save_data=True,
         first=False,
         middle_cut_out=False,
-        param=""):
+        param="",
+        overwrite=False):
+    print("========")
+    print("Autoencoder : \t\t", Autoencoder.getType(), "\t", Autoencoder.getName())
+    print("Dataloader:   \t\t", Dataloader.get_name())
+    print("Clustering:   \t\t", Classifier.get_name())
+    print("Param : \t\t", param)
+    print("========")
 
+    my_dataloader = Dataloader.get_dataloader(verbal=False)
     the_image_shape = Dataloader.get_image_shape()
     # (x, y, z)
     the_image_list = Dataloader.get_image_list()
@@ -101,7 +109,8 @@ def run_machine(
     params = list(my_net.parameters())
     print("Params size:  ", params.__len__())
     for parameter in params:
-        print(len(parameter))
+        print(len(parameter), end="  ||  ")
+    print()
 
     dataloader_suffix = ""
     if "cut_out" in Dataloader.get_name():
@@ -155,8 +164,6 @@ def run_machine(
             print("No multiple GPUs")
         my_net.to(device)
 
-        my_dataloader = Dataloader.get_dataloader(verbal=False)
-
         from torch.autograd import Variable
         num_epochs = 10
         batch_size = 128
@@ -189,10 +196,41 @@ def run_machine(
                     the_best_loss,
                     Dataloader.get_name())
 
+
+    print()
+    print("***   If overwrite data   ***")
+    print("---------------------------------")
+    print("Overwrite: ", overwrite)
+    suffix = ""
+    if middle_cut_out:
+        suffix = "_mcu"
+    img_name = \
+        Dataloader.get_name() + "_" + \
+        Classifier.get_name() + "_" + \
+        my_net.getType() + '_autoencoder_' + \
+        my_net.getName() + suffix + '.png'
+    result_img_path = Dataloader.get_results_directory() + 'img/' + img_name
+    data_name = \
+        Dataloader.get_name() + "_" + \
+        Classifier.get_name() + "_" + \
+        my_net.getType() + '_autoencoder_' + \
+        my_net.getName() + suffix + '.txt'
+    result_data_path = Dataloader.get_results_directory() + 'data/' + data_name
+    print("Result file names:")
+    print(result_img_path)
+    print(result_data_path)
+    if not overwrite and \
+        os.path.isfile(result_img_path) and \
+        os.path.isfile(result_data_path):
+        print("Result files exist ")
+        print("Ending procedure [*]")
+        return
+
     print()
     print("***   Autoencoding immage   ***")
     print("---------------------------------")
     print("Image shape: ", the_image_shape)
+    print("Image list shape: ", np.shape(the_image_list))
     print("Image code got from autoencoder")
     my_net.to(torch.device("cpu"))
     the_image_autoencoded = [my_net.getCode(torch.Tensor(point)).detach().numpy() for point in the_image_list]
@@ -206,7 +244,8 @@ def run_machine(
     print()
     print("***   Clustering   ***")
     print("---------------------------------")
-    the_image_classified = Classifier.clustering(the_image_autoencoded, the_image_shape, nr_of_clusters, param)
+    the_image_classified =\
+        Classifier.clustering(the_image_autoencoded, the_image_shape, nr_of_clusters, extra_parameters=param)
     print("Result shape: ", np.shape(the_image_classified))
 
     print()
@@ -222,12 +261,6 @@ def run_machine(
     print("---------------------------------")
     if save_img:
         plt.imshow(the_image_classified)
-        suffix = ""
-        if middle_cut_out:
-            suffix = "_mcu"
-
-        img_name = Classifier.get_name() + "_" + my_net.getType() + '_autoencoder_' + my_net.getName() + suffix + '.png'
-        result_img_path = Dataloader.get_results_directory() + 'img/' + img_name
         print("Path: ", result_img_path)
         plt.savefig(result_img_path, bbox_inches='tight')
 
@@ -235,8 +268,6 @@ def run_machine(
     print("***   Saving data   ***")
     print("---------------------------------")
     if save_data:
-        data_name = Classifier.get_name() + "_" + my_net.getType() + '_autoencoder_' + my_net.getName() + '.txt'
-        result_data_path = Dataloader.get_results_directory() + 'data/' + data_name
         print("Path: ", result_data_path)
         np.savetxt(result_data_path, the_image_classified, delimiter=" ", newline="\n", header=data_name, fmt="%s")
 
@@ -251,51 +282,41 @@ def run_machine_for_all():
     autoencoders.append(Autoencoder1)
     autoencoders.append(Autoencoder2)
     autoencoders.append(Autoencoder3)
-    # autoencoders.append(Autoencoder4)
-    # autoencoders.append(Autoencoder5)
+    autoencoders.append(Autoencoder4)
+    autoencoders.append(Autoencoder5)
 
     dataloaders = []
     dataloaders.append(Dataloader1)
-    dataloaders.append(Dataloader11)
-    dataloaders.append(Dataloader2)
-    dataloaders.append(Dataloader3)
-    dataloaders.append(Dataloader33)
-    dataloaders.append(Dataloader4)
-    dataloaders.append(Dataloader44)
-    dataloaders.append(Dataloader5)
-    dataloaders.append(Dataloader55)
-    dataloaders.append(Dataloader6)
+    # dataloaders.append(Dataloader11)
+    # dataloaders.append(Dataloader2)
+    # dataloaders.append(Dataloader3)
+    # dataloaders.append(Dataloader33)
+    # dataloaders.append(Dataloader4)
+    # dataloaders.append(Dataloader44)
+    # dataloaders.append(Dataloader5)
+    # dataloaders.append(Dataloader55)
+    # dataloaders.append(Dataloader6)
 
     clustering_methods = []
     clustering_methods.append(classifier1)
-    clustering_methods.append(classifier2)
+    # clustering_methods.append(classifier2)
     # clustering_methods.append(classifier3)
     # clustering_methods.append(classifier4)
 
     for Dataloader in dataloaders:
         for Autoencoder in autoencoders:
             for clustering in clustering_methods:
-                print("========")
-                print("Autoencoder : \t\t", Autoencoder.getType(), "\t", Autoencoder.getName())
-                print("Dataloader:   \t\t", Dataloader().get_name())
-                print("Clustering:   \t\t", clustering.get_name())
-                print("========")
                 if clustering == classifier2:
                     for shape in classifier2.cv_types:
-                        print("========")
-                        print("Shape : \t\t", shape)
-                        print("========")
-                        print("KOOTEK")
-                        # run_machine(Autoencoder, Dataloader(), clustering, Dataloader.get_number_of_clusters()
-                        #             , first=False, middle_cut_out=True, param=shape)
-                        # run_machine(Autoencoder, Dataloader(), clustering, Dataloader.get_number_of_clusters()
-                        #             , first=False, middle_cut_out=False, param=shape)
+                        run_machine(Autoencoder, Dataloader(), clustering, Dataloader.get_number_of_clusters()
+                                    , first=False, middle_cut_out=True, param=shape)
+                        run_machine(Autoencoder, Dataloader(), clustering, Dataloader.get_number_of_clusters()
+                                    , first=False, middle_cut_out=False, param=shape)
                 else:
-                    print("koot")
-                    # run_machine(Autoencoder, Dataloader(), clustering, Dataloader.get_number_of_clusters()
-                    #             , first=False, middle_cut_out=True)
-                    # run_machine(Autoencoder, Dataloader(), clustering, Dataloader.get_number_of_clusters()
-                    #             , first=False, middle_cut_out=False)
+                    run_machine(Autoencoder, Dataloader(), clustering, Dataloader.get_number_of_clusters()
+                                , first=False, middle_cut_out=True)
+                    run_machine(Autoencoder, Dataloader(), clustering, Dataloader.get_number_of_clusters()
+                                , first=False, middle_cut_out=False)
 
     print()
     print("***   Run machine for all end   ***")
@@ -321,8 +342,8 @@ if __name__ == '__main__':
     run_machine_for_all()
 
     # run_machine(
-    #     Autoencoder1, Dataloader55(), classifier2, Dataloader55.get_number_of_clusters(),
-    #     first=False, middle_cut_out=False, param='tied')
+    #      Autoencoder5, Dataloader5(), classifier1, Dataloader5.get_number_of_clusters(),
+    #      first=False, middle_cut_out=False, param='tied')
 
     # end procedures
 
@@ -337,4 +358,3 @@ if __name__ == '__main__':
         output_file.close()
 
     plt.show()
-
