10d3e3bf91ed84d56fb42630960359d0a6f6947a
Michał Huras <michal.huras94@gmail.com>
2019-09-28 10:37:27 +0200

    Parameters for kmeans

--- venv/clustering/kmeans.py
+++ venv/clustering/kmeans.py
@@ -19,7 +19,18 @@ def clustering(the_image_autoencoded, the_image_shape, number_of_clusters, extra
     df = DataFrame(data=the_image_autoencoded)
 
     print("KMeans clustering")
-    kmeans = KMeans(n_clusters=number_of_clusters).fit(df)
+    kmeans = KMeans(
+            n_clusters=number_of_clusters,
+            init="k-means++",
+            n_init=10,
+            max_iter=300,
+            tol=0.0001,
+            precompute_distances=True,
+            verbose=1,
+            random_state=42, # Answer to the Ultimate Question of Life, the Universe, and Everything (42)
+            copy_x=True,
+            n_jobs=None,
+            algorithm="elkan").fit(df)
 
     print("Creating list for clustered data")
     clustered_data = np.zeros((the_image_shape[0], the_image_shape[1]))
